54eed93ecd5deec606768d74c5493bf42f8db900
Felix Kazoshima <114717531+CauseIWont@users.noreply.github.com>
2023-01-19 22:32:45 -0600

    Update truck.js

--- truck.js
+++ truck.js
@@ -4,7 +4,7 @@ if (geofs.aircraft.instance.id == 2) {
 	setInterval(function(){if (geofs.aircraft.instance.id != 2) {geofs.addonAircraft.isTruck = 0}},100)
 	//nosewheel
 	geofs.aircraft.instance.definition.parts[16].animations[0].value = "roll"
-	geofs.aircraft.instance.definition.parts[16].animations[0].ratio = 35
+	geofs.aircraft.instance.definition.parts[16].animations[0].ratio = 15
 	//airfoils
 	geofs.aircraft.instance.definition.parts[2].area = 0.069
 	geofs.aircraft.instance.definition.parts[3].area = 0.069
@@ -16,14 +16,14 @@ if (geofs.aircraft.instance.id == 2) {
 	geofs.aircraft.instance.definition.parts[9].area = 0.069
 	geofs.aircraft.instance.definition.parts[10].area = 1
    //thrust and Vspeeds
-	geofs.aircraft.instance.definition.parts[19].thrust = 2000
+	geofs.aircraft.instance.definition.parts[19].thrust = 1500
 	geofs.aircraft.instance.definition.Vspeeds = {"VS0": 0, "VS": 0, "VFE": 0, "VNO": 70, "VNE": 120}
 	//yoke anims
 	geofs.aircraft.instance.cockpitSetup.parts[10].animations[0].ratio = 0.0069
 	geofs.aircraft.instance.cockpitSetup.parts[11].animations[0].ratio = 0.0069
 	geofs.aircraft.instance.cockpitSetup.parts[16].animations[1].threshold = -1
 	//CG adjust (whyyyy doesn't this work?)
-	geofs.aircraft.instance.definition.parts[0].centerOfMass[1] = [2, 2, -2]
+	geofs.aircraft.instance.definition.parts[0].centerOfMass[1] = [0, 2, -2]
 } else {
    throw("Please switch to a Cessna 172 before running this mod")
 }
